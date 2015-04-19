63d5aa5bd7a96e2518f5b07d34876f9917164ccf
jack8409201 <jack8409201@gmail.com>
2015-04-19 23:05:00 +0800

    Modify Test class
    新增註解

--- Test.java
+++ Test.java
@@ -5,19 +5,22 @@
  */
 import java.util.Scanner;
 public class Test {
-
+	//main method 
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
 		
+		//Create MyMyStack class
 		MyStack MyStack1 = new MyStack();
 		
+		//Enter five strings and store in ArrayList
 		for(int i = 0; i < 5; i++){
 			System.out.print("Please enter a string : ");
 			String scanner = input.next();
 			MyStack1.push(scanner);
 		}
 		
+		//When the ArrayList isn't empty display
 		while(!MyStack1.isEmpty()){
 			System.out.print(MyStack1.pop()+" ");
 		}
