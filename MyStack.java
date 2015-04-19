2890cd366ab04b1db84db34948691ff8ba9294d4
jack8409201 <jack8409201@gmail.com>
2015-04-19 22:56:31 +0800

    Modify MyStack class
    新增註解

--- MyStack.java
+++ MyStack.java
@@ -7,33 +7,39 @@ import java.util.ArrayList;
 
 public class MyStack extends ArrayList<String>{
 	
+	//Create an ArrayList store objects
 	private ArrayList<String> list = new ArrayList<String>();
 
+	//Bollean method if stack is empty that return true
 	public boolean isEmpty() {
 		return super.isEmpty();
 	}
 
+	//Method return amount of element in the stack
 	public int getSize() {
 		return super.size();
 	}
 
+	//Method return top object of the stack but not delete
 	public Object peek() {
 		return super.get(getSize() - 1);
 	}
 
+	//Method delete top object in the stack and return it
 	public Object pop() {
 		Object o = super.get(getSize() - 1);
 		super.remove(getSize() - 1);
 		return o;
 	}
 
+	//Method store object on the top 
 	public void push(String o) {
 		super.add(o);
 	}
 
+	//Override the toString in the Object class
 	@Override
 	public String toString() {
 		return "stack: " + super.toString();
 	}
 }
-
